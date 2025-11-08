7b0fb65004e966a27081428941310a35305056bd
Sushanth M S <sushanthms20@gmail.com>
2025-11-08 18:27:21 +0530

    Hosting

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,14 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://emailonebox-project.onrender.com/api',
+    'https://emailoneboxproject.netlify.app/'
+  ],
+  credentials: true
+}));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../frontend')));
 
